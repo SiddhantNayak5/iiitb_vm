e018b2c8979174eb01a9112663a4a1e61a127c3b
SiddhantNayak5 <110079689+SiddhantNayak5@users.noreply.github.com>
2022-08-13 22:21:04 +0530

    Add files via upload

--- iiitb_vm_tb.v
+++ iiitb_vm_tb.v
@@ -1,28 +1,34 @@
 module iiitb_vm_tb;
-//inputs
-reg clk;
-reg[1:0] in;
-reg rst;
-//output
-wire out;
-wire[1:0] change;
-iiitb_vm uut(
-.clk(clk),
-.rst(rst),
-.in(in),
-.out(out),
-.change(change)
-);
-initial begin
-//initialise inputs
-  $dumpfile("iiitb_vm.vcd");
+
+  //inputs
+  reg clk;
+  reg[1:0] in;
+  reg rst;
+
+  //output
+  wire out;
+  wire[1:0] change;
+
+ iiitb_vm uut(
+  .clk(clk),
+  .rst(rst),
+  .in(in),
+  .out(out),
+  .change(change)
+ );
+
+ initial begin
+   //initialise inputs
+$dumpfile("iiitb_vm.vcd");
   $dumpvars(0,iiitb_vm_tb);
-rst = 1;
-clk = 0;
-#6 rst = 0;
-in = 2;
-#19 in = 2;
-#25 $finish;
-end
-always #5 clk = ~clk;
+  rst = 1;
+  clk = 0;
+
+  #6 rst = 0;
+  in = 2;
+  #19 in = 2;
+  #25 $finish;
+
+ end
+ always #5 clk = ~clk;
 endmodule
