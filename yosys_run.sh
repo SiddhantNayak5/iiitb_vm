b572d8c28c496d6172baeea39a87601e9c18b6ec
SiddhantNayak5 <110079689+SiddhantNayak5@users.noreply.github.com>
2022-08-16 21:43:13 +0530

    Add files via upload

--- yosys_run.sh
+++ yosys_run.sh
@@ -4,11 +4,15 @@ read_verilog iiitb_vm.v
 synth -top iiitb_vm
 
 # mapping to mycells.lib
-dfflibmap -liberty /home/siddhant/asic/sky130RTLDesignAndSynthesisWorkshop-main/my_lib/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+dfflibmap -liberty /home/siddhant/asic/iiitb_vm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+proc
+opt
+abc -liberty /home/siddhant/asic/iiitb_vm/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 
-abc -liberty /home/siddhant/asic/sky130RTLDesignAndSynthesisWorkshop-main/my_lib/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-
-clean
+proc
 flatten
+clean
 # write synthesized design
 write_verilog iiitb_vm_synth.v
+stat
+show
